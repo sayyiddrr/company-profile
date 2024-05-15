f40c9ae67988a0bbc3379819074ece935429b08e
sayyid <tarjikamal@gmail.com>
2024-05-15 16:13:28 +0700

    halaman template admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\NamaController;
 
 Route::get('/', function () {
-    return view('index');
+    return view('admin');
 });
+
+// Route::get('', [App\Http\Controllers\NamaController::class, ''])->name('');
+// Route::get('', [NamaController:class, ''])->name('');
